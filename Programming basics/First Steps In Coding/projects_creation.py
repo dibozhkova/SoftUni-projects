6100c8b592bb8d6bc938be7b75ab39cea51df7dd
Denitsa Bozhkova <denica.bojkova@gmail.com>
2022-08-25 18:11:30 +0300

    Add files via upload

---
+++ Programming basics/First Steps In Coding/projects_creation.py
@@ -0,0 +1,3 @@
+name_of_the_architect = input()
+number_of_project_to_prepare = int(input())
+print(f"The architect {name_of_the_architect} will need {number_of_project_to_prepare * 3} hours to complete {number_of_project_to_prepare} project/s.")
