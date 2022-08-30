89991de1da88a0bb5cbd832b8c64e6ebc4027abf
Denitsa Bozhkova <denica.bojkova@gmail.com>
2022-08-30 15:28:46 +0300

    Add files via upload

---
+++ Programming basics/Conditional Statements Advanced/on_time_for_the_exam.py
@@ -0,0 +1,8 @@
+hour_for_exam = int(input())
+minutes_for_exam = int(input())
+hour_for_arrival = int(input())
+minutes_for_arrival = int(input())
+if hour_for_exam == hour_for_arrival\
+    and minutes_for_exam == minutes_for_exam <= 30:
+    print("On time")
+elif minutes_for_exam < minutes_for_exam - 30
