110e942992df7d4216db5464fd09cbf651611637
Denitsa Bozhkova <denica.bojkova@gmail.com>
2022-09-05 10:19:11 +0300

    Create min_number.py

---
+++ Programming basics/While Loop/min_number.py
@@ -0,0 +1,11 @@
+import sys
+
+number = input()
+min_number = sys.maxsize
+
+while number != "Stop":
+    current_number = int(number)
+    if current_number < min_number:
+        min_number = current_number
+    number = input()
+print(f"{min_number}")
