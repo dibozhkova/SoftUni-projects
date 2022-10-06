02ec646e088b7e517329fcda000aa78b95c069b5
Denitsa Bozhkova <denica.bojkova@gmail.com>
2022-10-06 22:09:37 +0300

    Create easter_gifts.py

---
+++ Programming Fundamentals/Lists Basics/easter_gifts.py
@@ -0,0 +1,19 @@
+gifts = input().split(" ")
+command = input().split(" ")
+while command[0] != "No" and command[1] != "Money":
+    index = 0
+    if command[0] == "OutOfStock":
+        current_gift = command[1]
+        gifts = list(map(lambda x: x.replace(current_gift, "None"), gifts))
+    elif command[0] == "Required":
+        index = int(command[2])
+        if 0 < index <= len(gifts):
+            gifts[index] = command[1]
+    elif command[0] == "JustInCase":
+        gifts[-1] = command[1]
+
+    command = input().split(" ")
+while "None" in gifts:
+    gifts.remove("None")
+for element in gifts:
+    print(element, end = " ")
